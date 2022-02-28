766baaf3dcc3c73ae200d8b1c1c6a003e9cce7f7
AYOUB EL MAMOUN <92216706+Ayoub-Elmamoun@users.noreply.github.com>
2022-02-28 08:36:57 +0000

    Delete Prototypes directory

--- Prototypes/prototype2/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-   $conn = mysqli_connect('localhost', 'ayoub', 'ayoub!123', 'demo');
-
-   // check connection
- if(!$conn){
-      echo 'Connection error: ' . mysqli_connect_error(); 
-  }
-?>
