60cd4312a5eaabe8d6bea0abfebfe02c8093db91
znwafae <92216706+ayoub-codes@users.noreply.github.com>
2022-03-25 10:14:25 +0000

    get data done !

--- Prototypes/prototype3/employeeManager.php
+++ Prototypes/prototype3/employeeManager.php
@@ -4,10 +4,24 @@ require_once 'employee.php';
 class employeeManager {
     private $connection = null;
 
-    private function getConnection (){
-        
-    }
+    // check connection
 
+    private function getConnection (){
+       if (is_null($this->connection)) {
+           $this->connection = mysqli_connect('localhost','admin','admin','employee_db');
+           if (!$this->connection) {
+               $message = 'connection failed'. mysqli_connect_error();
+               throw new Exception($message);
+           }
+       }
+       return $this->connection; 
+    }
+    
+    public function getAllEmployee(){
+        // get data from data base
+        $getdata = 'SELECT * FROM employeeinfo';
+
+    }
 }
 
 ?>
