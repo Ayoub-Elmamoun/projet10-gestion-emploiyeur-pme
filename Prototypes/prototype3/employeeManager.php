80f3b84558055265ce3cc61e0c9bf70e19ee8908
znwafae <92216706+ayoub-codes@users.noreply.github.com>
2022-03-25 11:14:29 +0000

    added

--- Prototypes/prototype3/employeeManager.php
+++ Prototypes/prototype3/employeeManager.php
@@ -8,6 +8,7 @@ class employeeManager {
 
     private function getConnection (){
        if (is_null($this->connection)) {
+           // connect to database
            $this->connection = mysqli_connect('localhost','admin','admin','employee_db');
            if (!$this->connection) {
                $message = 'connection failed'. mysqli_connect_error();
@@ -18,10 +19,28 @@ class employeeManager {
     }
     
     public function getAllEmployee(){
-        // get data from data base
+        // select data from data base
         $getdata = 'SELECT * FROM employeeinfo';
+        //make query and get result
+        $result = mysqli_query($this->getConnection(),$getdata);
+        //get data as array
+        $employeeList = mysqli_fetch_assoc($result);
+        print_r($employeeList);
+        $employees = array();
 
+        foreach ($employeeList as $employeeList) {
+            $employee = new Employee();
+            $employee->setId($employeeList["id"]);
+            $employee->setFirstName($employeeList["firstname"]);
+            $employee->setLastName($employeeList["lastname"]);
+            $employee->setGender($employeeList["gender"]);
+            $employee->setBirthDate($employeeList["birthdate"]);
+            array_push($employees,$employee);
+        }
+        return $employees;
     }
+
+
 }
 
 ?>
