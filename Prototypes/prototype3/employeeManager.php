925db83840e0c786b6accd92d31301ca701a3ba4
Ayoub-Elmamoun <mamounayoub@protonmal.com>
2022-03-28 05:12:02 +0100

    prototypes finished

--- Prototypes/prototype3/employeeManager.php
+++ Prototypes/prototype3/employeeManager.php
@@ -1,45 +1,90 @@
 <?php
-
 require_once 'employee.php';
-class employeeManager {
+
+class EmployeeManager { 
     private $connection = null;
 
-    // check connection
+    private function getConnection(){
+        if (is_null($this->connection)){
+            $this->connection = mysqli_connect('localhost', 'test', 'test123', 'demo');
 
-    private function getConnection (){
-       if (is_null($this->connection)) {
-           // connect to database
-           $this->connection = mysqli_connect('localhost','ayoub','ayoub!123','employees_db');
-           if (!$this->connection) {
-               $message = 'connection failed'. mysqli_connect_error();
-               throw new Exception($message);
-           }
-       }
-       return $this->connection; 
+            if(!$this->connection){
+                $message = 'Connection failed ' . mysqli_connect_error();
+                throw new Exception($message);
+            }
+        }
+      return $this->connection;
     }
     
-    public function getAllEmployee(){
-        // select data from data base
-        $getdata = 'SELECT * FROM employeeinfo';
-        //make query and get result
-        $result = mysqli_query($this->getConnection(),$getdata);
-        //get data as array
-        $employeeList = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    public function getAllEmployees(){
+        $sqlGetData = 'SELECT id, firstName, lastName, birthDate, gender FROM employees_db1';
+        $result = mysqli_query($this->getConnection(), $sqlGetData);
+        $employeesList = mysqli_fetch_assoc($result);
+
         $employees = array();
 
-        foreach ($employeeList as $employeeList) {
+        foreach($employeesList as $employeeList){
             $employee = new Employee();
-            $employee->setId($employeeList["id"]);
-            $employee->setFirstName($employeeList["firstname"]);
-            $employee->setLastName($employeeList["lastname"]);
-            $employee->setGender($employeeList["gender"]);
-            $employee->setBirthDate($employeeList["birthdate"]);
-            array_push($employees,$employee);
+            $employee->setId($employeeList['id']);
+            $employee->setFirstName($employeeList['firstName']);
+            $employee->setLastName($employeeList['lastName']);
+            $employee->setBirthDate($employeeList['birthDate']);
+            $employee->setGender($employeeList['gender']);
+            array_push($employees, $employee);  
         }
         return $employees;
     }
 
+    public function insertEmployee($employee){
+        $firstName = $employee->getFirstName();
+        $lastName = $employee->getLastName();
+        $birthDate = $employee->getBirthDate();
+        $gender = $employee->getGender();
 
+        // sql insert query
+        $sqlInsertQuery = "INSERT INTO employees_db1(firstName,lastName, birthDate, gender)
+                           VALUES('$firstName', '$lastName', '$birthDate', '$gender')";
+                           
+        mysqli_query($this->getConnection(), $sqlInsertQuery);
+    }
+        // sql delete query
+        public function deleteEmployee($id){
+            $sqlDeleteQuery = "DELETE FROM employees_db1 WHERE id=$id";
+            mysqli_query($this->getConnection(), $sqlDeleteQuery);
+        }
+    
+        public function editEmployee($id, $firstName, $lastName, $birthDate, $gender){
+            // Update Query
+            $sqlUpdateQuery = "UPDATE employees_db1 SET
+                              firstName='$firstName', lastName='$lastName', birthDate='$birthDate', gender='$gender'
+                              WHERE id=$id";
+            // Make Query
+            mysqli_query($this->getConnection(), $sqlUpdateQuery);
+            
+            if (mysqli_error($this->getConnection())){
+                $exceptionMessage = 'Error' . mysqli_errno($this->getConnection());
+                throw new Exception($exceptionMessage);
+            }
+                            
+        }
+
+        public function getEmployee($id){
+            $sqlGetQuery = "SELECT * FROM employees_db1 WHERE id=$id";
+    
+            // get result
+            $result = mysqli_query($this->getConnection(), $sqlGetQuery);
+    
+            // fetch to Associative array
+            $employee_data = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    
+            $employee = new Employee();
+            $employee->setId($employee_data['id']);
+            $employee->setFirstName($employee_data['firstName']);
+            $employee->setLastName($employee_data['lastName']);
+            $employee->setBirthDate($employee_data['birthDate']);
+            $employee->setGender($employee_data['gender']);
+    
+            return $employee;
+        }
 }
-
 ?>
--- Prototypes/prototype3/index.php
+++ Prototypes/prototype3/index.php
@@ -1,9 +1,8 @@
-<?php 
+<?php
+require_once 'employeeManager.php';
 
- require_once 'employeeManager.php';
-
- $employeeManager = new employeeManager;
- $data = $employeeManager->getAllEmployee();
+$employeeManager = new EmployeeManager();
+$data = $employeeManager->getAllEmployees();
 
 ?>
 
@@ -13,11 +12,36 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Index</title>
 </head>
 <body>
     <div>
-        <a href="insert.php">insert data</a>
+        <a href="insert.php">Insert Data</a>
+        <table>
+            <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Birth Date</th>
+                <th>Gender</th>
+                <th>Action</th>
+            </tr>
+
+            <?php
+                    foreach($data as $employee){
+            ?>
+
+            <tr>
+                <td><?= $employee->getFirstName()?></td>
+                <td><?= $employee->getLastName()?></td>
+                <td><?= $employee->getBirthDate()?></td>
+                <td><?= $employee->getGender()?></td>
+                <td>
+                    <a href="edit.php?id=<?php echo $employee->getId() ?>">Edit</a>
+                    <a href="delete.php?id=<?php echo $employee->getId() ?>">delete</a>
+                </td>
+            </tr>
+            <?php }?>
+        </table>
     </div>
 </body>
 </html>
