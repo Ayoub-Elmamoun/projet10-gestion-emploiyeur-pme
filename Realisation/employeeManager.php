3308119d1127ed061f96bf28fc9ff2fda541a50c
znwafae <92216706+ayoub-codes@users.noreply.github.com>
2022-06-02 10:18:58 +0100

    added

--- Realisation/employeeManager.php
+++ Realisation/employeeManager.php
@@ -1,23 +1,8 @@
 <?php
 include 'employee.php';
-
+include 'connection.php';
 class EmployeeManager
 {
-
-    private $Connection = null;
-
-    private function getConnection()
-    {
-        if (is_null($this->Connection)) {
-            $this->Connection = mysqli_connect('localhost', 'admin', 'admin', 'employees_db');
-
-            if (!$this->Connection) {
-                $message = 'Connection Error: ' . mysqli_connect_error();
-                throw new Exception($message);
-            }
-        }
-        return $this->Connection;
-    }
 
     public function getAllEmployees()
     {
